3c0fb7a6a010bea05d774dacfb585d508f915a32
joy-mahmud <joymahmudiucse@gmail.com>
2024-07-26 23:24:33 +0600

    scroll to botom for the messages partially done

--- app/(app)/chatRoom.jsx
+++ app/(app)/chatRoom.jsx
@@ -1,6 +1,6 @@
-import { View, Text, TextInput, TouchableOpacity, Alert, Keyboard } from 'react-native'
-import React, { useEffect, useRef, useState } from 'react'
-import { useLocalSearchParams, useRouter } from 'expo-router'
+import { View, Text, TextInput, TouchableOpacity, Alert, Keyboard } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import ChatRoomHeader from '../../components/ChatRoomHeader';
 import MessageList from '../../components/MessageList';
@@ -10,97 +10,94 @@ import CustomKeyboardAvoidingView from '../../components/CustomKeyboardAvoidingV
 import { useAuth } from '../../context/authContext';
 import { getRoomId } from '../../utils/common';
 import { addDoc, collection, doc, onSnapshot, orderBy, query, setDoc, Timestamp } from 'firebase/firestore';
-import { db, } from '../../firebaseConfig';
+import { db } from '../../firebaseConfig';
 
 const ChatRoom = () => {
-    const { user } = useAuth()
+    const { user } = useAuth();
     const item = useLocalSearchParams();
-    const [messages, setmessages] = useState([])
-    const router = useRouter()
-    const textRef = useRef('')
-    const inputRef = useRef(null)
-    const scrollViewRef = useRef(null)
+    const [messages, setMessages] = useState([]);
+    const router = useRouter();
+    const textRef = useRef('');
+    const inputRef = useRef(null);
+    const scrollViewRef = useRef(null);
 
     useEffect(() => {
         createRoomIfNotExists();
 
-        let roomId = getRoomId(user.uid, item.userId)
-        const docRef = doc(db, 'rooms', roomId)
-        const messageRef = collection(docRef, 'messages')
-        const q = query(messageRef, orderBy('createdAt', 'asc'))
+        const roomId = getRoomId(user.uid, item.userId);
+        const docRef = doc(db, 'rooms', roomId);
+        const messageRef = collection(docRef, 'messages');
+        const q = query(messageRef, orderBy('createdAt', 'asc'));
 
-        let unsubscribe = onSnapshot(q, (snapshot) => {
-            let allMessages = snapshot.docs.map((doc) => {
-                return doc.data();
-            })
-            setmessages([...allMessages])
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const allMessages = snapshot.docs.map((doc) => doc.data());
+            setMessages(allMessages);
+        });
 
-        })
-        const keyboardDidShowListener = Keyboard.addListener(
-            'keyboardDidShow',updateScrollView
-        )
-        return ()=>{
+        const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', updateScrollView);
+
+        return () => {
             unsubscribe();
             keyboardDidShowListener.remove();
-        }
-    }, [])
+        };
+    }, []);
+
     useEffect(() => {
-        updateScrollView()
-    }, [messages])
+        updateScrollView();
+    }, [messages]);
+
     const updateScrollView = () => {
-        setTimeout(() => {
-            scrollViewRef?.current?.scrollToEnd({ animated: true })
-        }, 100)
-    }
-    //console.log(messages)
+        if (scrollViewRef.current) {
+            scrollViewRef.current.scrollToEnd({ animated: true });
+        }
+    };
+
     const createRoomIfNotExists = async () => {
-        let roomId = getRoomId(user?.uid, item?.userId)
-        await setDoc(doc(db, "rooms", roomId), {
+        const roomId = getRoomId(user?.uid, item?.userId);
+        await setDoc(doc(db, 'rooms', roomId), {
             roomId,
             createdAt: Timestamp.fromDate(new Date())
-        })
-    }
+        });
+    };
+
     const handleSendMessage = async () => {
-        let message = textRef.current.trim()
+        const message = textRef.current.trim();
         if (!message) return;
         try {
-            let roomId = getRoomId(user?.uid, item.userId)
-            const docRef = doc(db, 'rooms', roomId)
-            const messageRef = collection(docRef, "messages")
-            textRef.current = ""
-            if (inputRef) inputRef?.current?.clear();
-            const newDoc = await addDoc(messageRef, {
+            const roomId = getRoomId(user?.uid, item.userId);
+            const docRef = doc(db, 'rooms', roomId);
+            const messageRef = collection(docRef, 'messages');
+            textRef.current = '';
+            if (inputRef.current) inputRef.current.clear();
+            await addDoc(messageRef, {
                 userId: user?.uid,
                 text: message,
                 profileUrl: item?.profileUrl,
                 senderName: user?.displayName,
                 createdAt: Timestamp.fromDate(new Date())
-            })
-            //console.log('mesage:',message)
+            });
         } catch (error) {
-            Alert.alert("Message", error.message)
+            Alert.alert('Message', error.message);
         }
-    }
-   
+    };
+
     return (
-
-        <View className='flex-1 bg-white'>
-            <StatusBar style='dark'></StatusBar>
-
-            <ChatRoomHeader user={item} router={router}></ChatRoomHeader>
+        <View className="flex-1 bg-white">
+            <StatusBar style="dark" />
+            <ChatRoomHeader user={item} router={router} />
             <View className="flex-1 justify-between bg-neutral-100 overflow-visible">
                 <View className="flex-1">
-                    <MessageList scrollVieRef={scrollViewRef} messages={messages} currentUser={user}></MessageList>
+                    <MessageList scrollViewRef={scrollViewRef} messages={messages} currentUser={user} />
                 </View>
                 <View style={{ marginBottom: hp(2.7) }} className="pt-2">
                     <View className="mx-3 flex-row justify-between bg-white border-[1px] border-neutral-400 rounded-full pl-5">
                         <TextInput
                             ref={inputRef}
-                            onChangeText={value => textRef.current = value}
-                            placeholder='Type your message ...'
+                            onChangeText={(value) => (textRef.current = value)}
+                            placeholder="Type your message ..."
                             style={{ fontSize: hp(2) }}
                             className="flex-1 font-medium mr-2 h-[40px] text-neutral-800"
-                        ></TextInput>
+                        />
                         <TouchableOpacity onPress={handleSendMessage}>
                             <MaterialCommunityIcons name="send-circle" size={39} color="black" />
                         </TouchableOpacity>
@@ -108,8 +105,7 @@ const ChatRoom = () => {
                 </View>
             </View>
         </View>
+    );
+};
 
-    )
-}
-
-export default ChatRoom
+export default ChatRoom;
--- components/MessageList.jsx
+++ components/MessageList.jsx
@@ -1,29 +1,27 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-const MessageList = ({ messages, currentUser,scrollViewRef }) => {
+import { View, Text, ScrollView } from 'react-native';
+import React from 'react';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+
+const MessageList = ({ messages, currentUser, scrollViewRef }) => {
     return (
-        <ScrollView ref={scrollViewRef}  showsVerticalScrollIndicator={false} className="p-3 ">
-            {
-                messages.map((msg, index) => {
-                    if (currentUser?.uid == msg.userId) {
-                        return (
-                            <View key={index} className="p-2 rounded-md self-end bg-neutral-800 mb-2 " style={{maxWidth:wp(80)}}>
-                                <Text className="font-medium text-white" >{msg.text}</Text>
-                            </View>
-                        )
-                    } else {
-                        return (
-                            <View key={index} className="p-2 self-start rounded-md bg-neutral-200 mb-2" style={{maxWidth:wp(80)}}>
-                                <Text className="font-medium text-neutral-800" >{msg.text}</Text>
-                            </View>
-                        )
-                    }
-
-                })
-            }
+        <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false} className="px-3">
+            {messages.map((msg, index) => {
+                if (currentUser?.uid === msg.userId) {
+                    return (
+                        <View key={index} className="p-2 rounded-md self-end bg-neutral-800 mb-2" style={{ maxWidth: wp(80) }}>
+                            <Text className="font-medium text-white">{msg.text}</Text>
+                        </View>
+                    );
+                } else {
+                    return (
+                        <View key={index} className="p-2 self-start rounded-md bg-neutral-200 mb-2" style={{ maxWidth: wp(80) }}>
+                            <Text className="font-medium text-neutral-800">{msg.text}</Text>
+                        </View>
+                    );
+                }
+            })}
         </ScrollView>
-    )
-}
+    );
+};
 
-export default MessageList
+export default MessageList;
